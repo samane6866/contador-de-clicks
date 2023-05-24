783b1db787278266c49d026335b02ffcde3cc679
samane6866 <samane.bahramian@gmail.com>
2023-05-24 19:27:15 +0200

    css button done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 import freeCodeCampLogo from "./imagenes/FreeCodeCamp_logo.png";
+import Boton from "./componentes/Boton";
 
 function App() {
+  const manejarClic = () => {
+    console.log("clic");
+  };
+  const reiniciarContador = () => {
+    console.log("Reiniciar");
+  };
+
   return (
     <div className="App">
       <div className="freecodecamp-logo-contenedor">
@@ -11,6 +19,12 @@ function App() {
           alt="Logo de freecodecamp"
         />
         <div className="contenedor-principal"></div>
+        <Boton texto="clic" esBotonDeClick={true} manejarClic={manejarClic} />
+        <Boton
+          texto="reiniciar"
+          esBotonDeClick={false}
+          manejarClic={reiniciarContador}
+        />
       </div>
     </div>
   );
--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Botoncss from "./hojas-de-estilo/Boton.css";
 
-function Boton({ texto }) {
-  return <button>{texto}</button>;
+function Boton({ texto, esBotonDeClick, manejarClic }) {
+  return (
+    <button
+      className={esBotonDeClick ? "boton-clic" : "boton-reiniciar"}
+      onClick={manejarClic}
+    >
+      {texto}
+    </button>
+  );
 }
+export default Boton;
